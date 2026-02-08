212f8a4fcc4f4c6c362d8758cba998b09014446b
Marcus Vinicius <marcusvgr30@gmail.com>
2026-02-08 22:56:58 +0100

    day10

---
+++ src/day10.rs
@@ -0,0 +1,129 @@
+use crate::utils::read_lines;
+use std::io;
+
+fn getting_input(input: &str) -> io::Result<Vec<Vec<u8>>>{
+
+    let lines = read_lines(input).expect("File not found");
+    let mut table: Vec<Vec<u8>> = Vec::new();
+    
+    for line in lines{
+        let line = line?;
+        let line: Vec<&str> = line.split("").collect();
+        let mut tmp: Vec<u8> = Vec::new();
+        for c in line{
+            if c.len() < 1{
+                continue;
+            }
+            tmp.push(c.parse::<u8>().expect("Problem in line"));
+        }
+        table.push(tmp);
+    }
+    
+    Ok(table)
+
+}
+
+
+fn solve_trailhead(n: u8, row: usize, col: usize, map: &Vec<Vec<u8>>, trail_ends: &mut Vec<(usize, usize)>){
+
+    if n == 9{
+        trail_ends.push((row, col));
+    }
+
+
+    else if n < 9{
+
+        if row > 0{
+            if map[row-1][col] == n + 1{
+                solve_trailhead(n+1, row-1, col, map, trail_ends);    
+            }
+
+        }
+        if row < map.len()-1{
+            if map[row+1][col] == n + 1{
+                solve_trailhead(n+1, row+1, col, map, trail_ends);    
+            }
+        }
+        if col > 0{
+            if map[row][col-1] == n + 1{
+                solve_trailhead(n+1, row, col-1, map, trail_ends);    
+            }
+
+        }
+        if col < map[0].len()-1{
+            if map[row][col+1] == n + 1{
+                solve_trailhead(n+1, row, col+1, map, trail_ends);    
+            }
+        }
+
+
+    }
+
+}
+
+fn size_unique_vector(V: Vec<(usize, usize)>) -> usize{
+
+    let mut set: Vec<(usize, usize)> = Vec::new();
+
+    for v in V{
+        if !set.contains(&v){
+            set.push(v);
+        }
+    }
+
+    set.len()
+
+}
+
+
+fn solution1(input: &str) -> io::Result<()>{
+    println!("Running Day10, sol1, {}", input);
+
+    let map: Vec<Vec<u8>> = getting_input(input).expect("Failed to get input");
+    let mut answer: u32 = 0;
+
+    for row in 0..map.len(){
+        for col in 0..map[0].len(){
+            if map[row][col] == 0{
+                let mut trail_ends: Vec<(usize, usize)> = Vec::new();
+                solve_trailhead(0, row, col, &map, &mut trail_ends);
+                //println!("{:?}", trail_ends);
+                answer += size_unique_vector(trail_ends) as u32;
+            }
+        }
+    }
+
+    println!("{answer}");
+
+    Ok(())
+}
+
+fn solution2(input: &str) -> io::Result<()>{
+    println!("Running Day10, sol2, {}", input);
+
+    let map: Vec<Vec<u8>> = getting_input(input).expect("Failed to get input");
+    let mut answer: u32 = 0;
+
+    for row in 0..map.len(){
+        for col in 0..map[0].len(){
+            if map[row][col] == 0{
+                let mut trail_ends: Vec<(usize, usize)> = Vec::new();
+                solve_trailhead(0, row, col, &map, &mut trail_ends);
+                answer += trail_ends.len() as u32;
+            }
+        }
+    }
+
+    println!("{answer}");
+
+    Ok(())
+}
+
+
+
+pub fn run() {
+    let _ = solution1("inputs/example10.txt");
+    let _ = solution1("inputs/input10.txt");
+    let _ = solution2("inputs/example10.txt");
+    let _ = solution2("inputs/input10.txt");
+}
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod day6;
 mod day7;
 mod day8;
 mod day9;
+mod day10;
 
 fn main() {
 
@@ -22,6 +23,7 @@ fn main() {
         day7::run,
         day8::run,
         day9::run,
+        day10::run,
     ];
 
     let day: usize = std::env::args() 
